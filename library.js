7f373aedf52e10796d2bd2879bc0b273b8aca66d
Louis Ouellet <louis_ouellet@hotmail.com>
2025-09-17 15:52:31 -0400

    General: Improved the development monitor.

---
+++ library.js
@@ -0,0 +1,16 @@
+API.success(function(response, endpoint){
+    API.debug(response.auth.development);
+    if(response.auth.development){
+        if(!$('i[data-dev-mode]').length){
+            $(document.createElement('i')).attr({"data-dev-mode": response.auth.development,"class": "position-fixed start-50 translate-middle-x bi bi-gear-wide text-info animate-fade", "style": "z-index:9999;font-size:5rem;bottom: 64px;"}).prependTo('body');
+            builder.Toast.add({
+                color: 'info',
+                icon: 'info-circle',
+                title: builder.Locale.get('Development Mode'),
+                body: builder.Locale.get('The application is currently running in development mode.'),
+            });
+        }
+    } else {
+        $('i[data-dev-mode]').remove();
+    }
+});
